6da7608dcf98b6e2def57f829df738da526bae48
James Chen <jamesc8453@gmail.com>
2025-09-02 22:31:25 -0400

    finished 07_reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,7 +1,7 @@
 const reverseString = function(phrase) {
-    let phraseArr = phrase.split();
+    let phraseArr = phrase.split("");
     phraseArr = phraseArr.reverse();
-    return phraseArr.join();
+    return phraseArr.join("");
 };
 
 // Do not edit below this line
